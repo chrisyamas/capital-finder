0d4433af57646764b1996a95aeeda122ba444579
Christopher Yamas <yamas.chris@gmail.com>
2022-05-10 20:29:32 -0400

    modules and docs complete

---
+++ README.md
@@ -0,0 +1,35 @@
+# LAB - Class 16
+
+## Project: Capital Finder
+
+### Author: Christopher Yamas
+
+### Setup
+
+#### How to run application
+
+The application is deployed on Vercel.app with the base url of:
+
+- https://capital-finder-eta.vercel.app/api/capital-finder
+
+### Examples
+
+#### Searching for country with capital name
+
+A user can query the app with a capital city name, like Paris, in the url:
+
+- https://capital-finder-eta.vercel.app/api/capital-finder?capital=paris
+
+This will render a string on the browser page saying:
+
+- Paris is the capital of France.
+
+#### Searching for capital with country name
+
+A user can also query the app with a country name, like France, in the url:
+
+- https://capital-finder-eta.vercel.app/api/capital-finder?capital=france
+
+This will render a string on the browser page saying:
+
+- The capital of France is Paris.
---
+++ api/capital-finder.py
@@ -0,0 +1,40 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        url_components = parse.urlsplit(self.path)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+
+        capital = dic.get("capital")
+        country = dic.get("country")
+
+        if country:
+            url = f"https://restcountries.com/v3.1/name/{country}"
+            response = requests.get(url)
+            data = response.json()
+            capital = data[0]["capital"][0]
+            country = country.title()
+            message = f"The capital of {country} is {capital}."
+        
+        elif capital:
+            url = f"https://restcountries.com/v3.1/capital/{capital}"
+            response = requests.get(url)
+            data = response.json()
+            country = data[0]["name"]["common"][0]
+            capital = capital.title()
+            message = f"{capital} is the capital of {country}."
+
+        else:
+            message = "There has been an error."
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
+        self.wfile.write(message.encode())
+
+        return
